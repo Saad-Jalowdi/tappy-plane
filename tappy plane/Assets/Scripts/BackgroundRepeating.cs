0b2e9e955708972222f92926de58a8b3406db6eb
SAAD <saad.tarek.jal@gmail.com>
2019-01-22 16:19:35 +0200

    initial commit
    a flappy bird similar game

---
+++ tappy plane/Assets/Scripts/BackgroundRepeating.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRepeating : MonoBehaviour {
+    public float groundHorizantalLength = 8;
+    Vector2 groundOffset;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x < -groundHorizantalLength)
+        {
+            repositionBackground();
+        }
+	}
+    public void repositionBackground()
+    {
+        groundOffset = new Vector2(groundHorizantalLength*2,0);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+}
---
+++ tappy plane/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlaneController : MonoBehaviour {
+    public bool isAlive = true;
+    public float thrust = 200f;
+    Animator anim;
+    public int Score = 0;
+    public Text scoretext; 
+    Rigidbody2D rb2d;
+    public GameObject gameOverText;
+    public GameObject scoreText;
+    public static PlaneController ins;
+    public void planeDestroyed()
+    {
+        rb2d.velocity = Vector2.zero;
+        transform.Rotate(0,0,0);
+        isAlive = false;
+        anim.SetBool("explode", true);
+        scoreText.SetActive(false);
+        gameOverText.SetActive(true);
+    }
+    private void Awake()
+    {
+        ins = this;
+    }
+    // Use this for initialization
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+      
+
+    }
+    public void jump()
+    {
+        if (isAlive)
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(new Vector2(0, thrust));
+        }else if (!isAlive)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        planeDestroyed();
+    }
+    public void score()
+    {
+        Score++;
+        scoretext.text = "SCORE : " + Score.ToString();
+    }
+}
